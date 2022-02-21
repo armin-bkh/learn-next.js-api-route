8c70b21ced034d5174b7d3e2ae092b31356275fb
arminbkh0921@yahoo.com <arminbkh0921@yahoo.com>
2022-02-21 19:26:11 +0330

    create GET api route

---
+++ database/comments.js
@@ -0,0 +1,14 @@
+export const comments = [
+    {
+        id: 1,
+        text: "that was greate",
+    },
+    {
+        id: 2,
+        text: "i love you",
+    },
+    {
+        id: 3,
+        text: "you should do it better",
+    }
+]
--- pages/comments/index.js
+++ pages/comments/index.js
@@ -1,15 +1,29 @@
+import { useState } from "react";
+
 const CommentsListPage = () => {
   const [comments, setComments] = useState([]);
-  
+
   const fetchCommentsHandler = async () => {
-    const data = await await fetch("/api/comments").json();
+    const response = await fetch("/api/comments");
+    const data = await response.json();
     console.log(data);
     setComments(data);
   };
 
   return (
     <>
-      <button type="button" onClick={fetchCommentsHandler}>load comments</button>
+      <button type="button" onClick={fetchCommentsHandler}>
+        load comments
+      </button>
+      {comments.length > 0 && (
+        <ul>
+          {comments.map((comment) => (
+            <li key={comment.id}>
+              <h2>{comment.text}</h2>
+            </li>
+          ))}
+        </ul>
+      )}
     </>
   );
 };
